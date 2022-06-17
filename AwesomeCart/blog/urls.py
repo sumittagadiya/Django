9f8addd053267339bc60b252e655d45978a13c95
myusername <myemail>
2022-06-17 16:02:40 +0530

    project done

--- AwesomeCart/blog/urls.py
+++ AwesomeCart/blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index,name="blogHome"),
+    path('blogpost/<int:id>', views.blogpost,name="blogPost")
 ]
